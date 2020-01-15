9da44a226731bad04474d5b6219213a1490e4e44
CesarJu <cesarju.cc@gmail.com>
2020-01-15 00:02:01 -0400

    Se agrego otro dado

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,6 +9,7 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     lateinit var diceImage : ImageView
+    lateinit var diceImage1 : ImageView
     lateinit var rollButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,17 +21,22 @@ class MainActivity : AppCompatActivity() {
         //De una manera mas optimizada declarando con lateinit para el cast cambia
         rollButton = findViewById(R.id.roll_button)
         diceImage = findViewById(R.id.dice_image)
+        diceImage1 = findViewById(R.id.dice_image1)
         //Llamada a la funcion del button
         rollButton.setOnClickListener { (rollDice()) }
-
-
     }
-    private fun rollDice(){
-        //Toast.makeText(this, "button click",Toast.LENGTH_LONG).show()
-        val randomInt = Random().nextInt(6) + 1
-
-
-        val drawableResourse = when (randomInt){
+    private fun rollDice() {
+        val randomInt1 = Random().nextInt(6)+1
+        val randomInt2 = Random().nextInt(6)+1
+            val drawableResourse = when (randomInt1) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+        val drawableResourse2 = when(randomInt2) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -38,7 +44,7 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.setImageResource(drawableResourse)
+            diceImage.setImageResource(drawableResourse)
+            diceImage1.setImageResource(drawableResourse2)
     }
-
 }
